78dff884e22515821c4c7a2bd7fc174dab9c9b59
david-borja <davidborjamartinez@gmail.com>
2026-07-11 14:45:32 +0200

    feat: implement book search functionality with error handling

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -3,14 +3,24 @@ import {jsx} from '@emotion/core'
 
 import './bootstrap'
 import Tooltip from '@reach/tooltip'
-import {FaSearch} from 'react-icons/fa'
+import {FaSearch, FaTimes} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
+import React from 'react'
+import { client } from 'utils/api-client.exercise'
+import * as colors from './styles/colors'
+import { useAsync } from 'utils/hooks'
 // 🐨 import the client from './utils/api-client'
 
 function DiscoverBooksScreen() {
+  const {data, error, run, isLoading, isError, isSuccess} = useAsync()
+  const [status, setStatus] = React.useState('idle')
+  const [query, setQuery] = React.useState('')
+  const [queried, setQueried] = React.useState(false)
+  // const [data, setData] = React.useState(null)
+  // const [error, setError] = React.useState(null)
   // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
+  // 💣 remove this, it's just here so the example doesn't explode
   // 🐨 you'll also notice that we don't want to run the search until the
   // user has submitted the form, so you'll need a boolean for that as well
   // 💰 I called it "queried"
@@ -22,15 +32,36 @@ function DiscoverBooksScreen() {
   // so you'll want to check if the user has submitted the form yet and if
   // they haven't then return early (💰 this is what the queried state is for).
 
+  console.log(query)
+
   // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  // const isLoading = status === 'loading'
+  // const isSuccess = status === 'success'
+  // const isError = status === 'error'
+
+  React.useEffect(() => {
+    if (!queried) {
+      return
+    }
+    setStatus('loading')
+    run(client(`books?query=${encodeURIComponent(query)}`))
+    // .then(responseData => {
+    //   setData(responseData)
+    //   setStatus('success')
+    // }, errorData => {
+    //   setError(errorData)
+    //   setStatus('error')
+    // })
+  }, [queried, query, run])
 
   function handleSearchSubmit(event) {
     // 🐨 call preventDefault on the event so you don't get a full page reload
     // 🐨 set the queried state to true
     // 🐨 set the query value which you can get from event.target.elements
     // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+    setQuery(event.target.elements.search.value)
+    setQueried(true)
   }
 
   return (
@@ -54,11 +85,18 @@ function DiscoverBooksScreen() {
                 background: 'transparent',
               }}
             >
-              {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
+              {isLoading ? <Spinner /> : (isError ? <FaTimes aria-label="error" css={{color: colors.danger}}/> : <FaSearch aria-label="search" />)}
             </button>
           </label>
         </Tooltip>
       </form>
+
+      {isError ? (
+        <div css={{ color: colors.danger }}>
+          <p>There was an error</p>
+          <pre>{error.message}</pre>
+        </div>
+      ) : null}
 
       {isSuccess ? (
         data?.books?.length ? (
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,4 +1,20 @@
 function client(endpoint, customConfig = {}) {
+  const config = {
+    method: 'GET',
+    ...customConfig,
+  }
+  return window.fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
+    .then(async (response) => {
+      console.log(response)
+      // return response.json()
+      const data = await response.json()
+      if (response.ok) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+
   // 🐨 create the config you'll pass to window.fetch
   //    make the method default to "GET"
   // 💰 if you're confused by this, that's fine. Scroll down to the bottom
